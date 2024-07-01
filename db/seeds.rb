cffed5b41d3d64ff42435dfe9897424d8f5c5901
LucDelmon <luc.delmon@gmail.com>
2024-07-01 17:15:31 +0200

    chore: add basic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,6 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# frozen_string_literal: true
+john = Author.create!(name: "John Doe")
+jane = Author.create!(name: "Jane Doe")
+Book.create!(title: "Book 1", page_count: 100, author: john)
+Book.create!(title: "Book 2", page_count: 200, author: jane)
+Book.create!(title: "Book 3", page_count: 30, author: john)

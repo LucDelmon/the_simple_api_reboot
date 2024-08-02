f11801fb2dc7e7cfaca8a9f807cf2a3f3b7242f1
LucDelmon <luc.delmon@gmail.com>
2024-08-02 14:45:57 +0200

    chore: update puma config

--- config/puma.rb
+++ config/puma.rb
@@ -20,11 +20,12 @@ if ENV['RAILS_ENV'] == 'production'
   workers worker_count if worker_count > 1
 end
 
+preload_app!
+
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
 worker_timeout 3600 if ENV.fetch('RAILS_ENV', 'development') == 'development'
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch('PORT', 3000)
 
 # Specifies the `environment` that Puma will run in.

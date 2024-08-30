b793702c0dea3436ef37dcd54038e8d6f3eba84d
LucDelmon <luc.delmon@gmail.com>
2024-08-30 12:29:19 +0200

    fix: add help controller spec to improve coverage

---
+++ spec/requests/help_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe '/help' do
+  describe 'GET /index' do
+    it 'renders a successful response' do
+      get(help_url, as: :json)
+      expect(response).to be_successful
+      expect(response.parsed_body).to include(
+        'endpoints' => a_kind_of(Hash)
+      )
+    end
+  end
+end
